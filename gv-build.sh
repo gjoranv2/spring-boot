75602091ade63746d0edd118c7d9ddbca4c39d31
gjoranv <gv@yahoo-inc.com>
2017-06-30 15:14:36 +0200

    Call travis_wait in travis script, not bash script.
    + Wrap maven commands in time.

--- gv-build.sh
+++ gv-build.sh
@@ -10,6 +10,6 @@ fi
 df -h
 df -h .
 
-travis_wait 45 ./mvnw install -T 2.0C -q -U -DskipTests=true -Pfast -Dmaven.test.redirectTestOutputToFile=true
+time ./mvnw install -T 2.0C -q -U -DskipTests=true -Pfast -Dmaven.test.redirectTestOutputToFile=true
 
-./mvnw install -T 2.0C -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration'
+time ./mvnw install -T 2.0C -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration'

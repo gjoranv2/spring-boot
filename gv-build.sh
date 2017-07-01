98b8f277e57d89e88b1e2bd00b104bc781995ac8
gjoranv <gv@yahoo-inc.com>
2017-07-01 20:05:43 +0200

    Use gv-build again, and with T=2.0C

--- gv-build.sh
+++ gv-build.sh
@@ -10,6 +10,6 @@ fi
 df -h
 df -h .
 
-time ./mvnw install -q -U -DskipTests=true -Pfast -Dmaven.test.redirectTestOutputToFile=true
+time ./mvnw install -T 2.0C -q -U -DskipTests=true -Pfast -Dmaven.test.redirectTestOutputToFile=true
 
-time ./mvnw install -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration'
+time ./mvnw install -T 2.0C -q -nsu -Dmaven.test.redirectTestOutputToFile=true -P '!integration'
